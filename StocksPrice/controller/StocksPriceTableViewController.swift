0d6757a463a8d44c276fb48793839c0b305747d4
Pran Kishore <kishorepran@gmail.com>
2020-09-27 09:30:49 +0530

    Create table view cell. conform to resusable for ease of use.

--- StocksPrice/controller/StocksPriceTableViewController.swift
+++ StocksPrice/controller/StocksPriceTableViewController.swift
@@ -12,35 +12,26 @@ class StocksPriceTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.register(cellType: StocksPriceTableViewCell.self)
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(for: indexPath) as StocksPriceTableViewCell
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
---
+++ StocksPrice/view/StocksPriceTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  StocksPriceTableViewCell.swift
+//  StocksPrice
+//
+//  Created by Pran Kishore on 27.09.20.
+//  Copyright © 2020 Sample Solutions. All rights reserved.
+//
+
+import Reusable
+
+class StocksPriceTableViewCell: UITableViewCell, NibReusable {
+
+    @IBOutlet weak var lblStockCode: UILabel!
+    @IBOutlet weak var lblStockName: UILabel!
+    @IBOutlet weak var lblStockPrice: UILabel!
+    @IBOutlet weak var lblStockChange: UILabel!
+    @IBOutlet weak var lblStockPostChange: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        setupStyle()
+    }
+    private func setupStyle() {
+        // Set up colors and font size
+    }
+}
