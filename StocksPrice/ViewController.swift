6a8990fd642efb6b60fb00bf0405fdef779cbad6
Pran Kishore <kishorepran@gmail.com>
2020-09-27 08:40:11 +0530

    Initial commit.

---
+++ StocksPrice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StocksPrice
+//
+//  Created by Pran Kishore on 27.09.20.
+//  Copyright © 2020 Sample Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
