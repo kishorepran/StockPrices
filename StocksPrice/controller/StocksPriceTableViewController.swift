5e791b49727b86f2de80bd5e5bcffdfc87a727c7
Pran Kishore <kishorepran@gmail.com>
2020-09-30 13:53:24 +0530

    Pull to refresh feature.

--- StocksPrice/controller/StocksPriceTableViewController.swift
+++ StocksPrice/controller/StocksPriceTableViewController.swift
@@ -17,8 +17,20 @@ class StocksPriceTableViewController: UITableViewController {
         setupView()
         viewModel.delegate = self
         viewModel.fetchQuotes()
+        setupRefreshControl()
     }
 
+    func setupRefreshControl() {
+        let refreshControl = UIRefreshControl()
+        tableView.refreshControl = refreshControl
+        refreshControl.tintColor = SPColorType.darkRed.color
+        refreshControl.attributedTitle = NSAttributedString.attributedString(text: "Fetching stock Quotes ...", style: SPStyle.elementDescription)
+        refreshControl.addTarget(self, action: #selector(refreshQuotesData(_:)), for: .valueChanged)
+    }
+    @objc private func refreshQuotesData(_ sender: Any) {
+        viewModel.fetchQuotes()
+    }
+    
     func setupView() {
         title = "Stocks Chart"
         tableView.register(cellType: StocksPriceTableViewCell.self)
@@ -70,6 +82,7 @@ class StocksPriceTableViewController: UITableViewController {
 extension StocksPriceTableViewController: ViewModelDelegate {
     func viewModelDidUpdate(sender: SPBaseViewModel) {
         tableView.reloadData()
+        self.refreshControl?.endRefreshing()
     }
     
     func viewModelUpdateFailed(error: SPError) {
